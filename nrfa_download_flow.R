56747d0c5e960c2d2f599e4af9a8761b183b6e70
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2022-07-07 16:55:10 +0100

    corrected NRFA so it starts in 2010-01-01

--- nrfa_download_flow.R
+++ nrfa_download_flow.R
@@ -1,5 +1,6 @@
 # Get the information for a single station
 
-library(nrfa)
+library(rnrfa)
 flowdata <- gdf(id = "21007")
+flowdata <- window(flowdata, start=as.Date("2010-01-01"))
 write.table(flowdata, "obs.txt", row.names=FALSE, col.names=FALSE)

2fe3d54e648dc7424e4832a3ac77fccd7a948cb0
Roy Sanderson <roy.sanderson@ncl.ac.uk>
2022-07-12 17:03:45 +0100

    Basic setup without calibration

--- nrfa_download_flow.R
+++ nrfa_download_flow.R
@@ -1,6 +1,12 @@
+#!/usr/bin/env Rscript
 # Get the information for a single station
 
 library(rnrfa)
-flowdata <- gdf(id = "21007")
-flowdata <- window(flowdata, start=as.Date("2010-01-01"))
-write.table(flowdata, "obs.txt", row.names=FALSE, col.names=FALSE)
+args <- commandArgs(trailingOnly = TRUE)
+nrfa_station_id <- args[1]
+start_date      <- args[2]
+end_date        <- args[3]
+out_file        <- args[4]
+flowdata <- gdf(id = nrfa_station_id)
+flowdata <- window(flowdata, start=as.Date(start_date))
+write.table(flowdata, out_file, row.names=FALSE, col.names=FALSE)

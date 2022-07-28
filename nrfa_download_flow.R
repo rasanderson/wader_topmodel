cbf604c73a9a4829c5e850699f5e44937033a632
Roy Sanderson <roy.sanderson@ncl.ac.uk>
2022-07-28 14:08:03 +0100

    Updated to use 1km PET data to 2017; code tidied as far as creation of input_pet.txt

--- nrfa_download_flow.R
+++ nrfa_download_flow.R
@@ -8,7 +8,7 @@ start_date      <- args[2]
 end_date        <- args[3]
 out_file        <- args[4]
 flowdata <- gdf(id = nrfa_station_id)
-flowdata <- window(flowdata, start=as.Date(start_date))
+flowdata <- window(flowdata, start=as.Date(start_date), end=as.Date(end_date))
 # NRFA flow data is in m3/seconds but topmodel needs m3/day
 # Multiply by 24*60*60 = 86400
 flowdata <- flowdata * 86400

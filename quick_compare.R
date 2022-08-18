48bddbbf9907762f555e066b137c7f3f0f191783
Roy Sanderson <roy.sanderson@ncl.ac.uk>
2022-08-18 20:53:02 +0100

    Completed saved runs on edge with nrfa
    These were previously calibrated. Note that observed vs predicted
    plots from quick_compare.R do not exactly match original
    calibration plots as burnin ignored, but broadly OK.

--- quick_compare.R
+++ quick_compare.R
@@ -2,9 +2,10 @@
 library(lubridate)
 library(ggplot2)
 library(tidyr)
+library(dplyr)
 
-subcatch_no <- 3
-nrfa_no     <- 21027
+subcatch_no <- 26
+nrfa_no     <- 21012
 
 preds <- read.table(paste0("subcatch_dat/output.txt.", subcatch_no),
                     skip=66, header=TRUE)
@@ -14,8 +15,15 @@ date  <- seq(ymd('2010-01-01'),ymd('2017-12-31'),by='days')
 
 topmod <- cbind(preds, nrfa, date)
 colnames(topmod)[10] <- "nrfa"
-plot(1:nrow(topmod), topmod$Qt, type="l")
-lines(topmod$nrfa, col="red")
+par(mar=c(5,4.5,4,2) + 0.1)
+plot(1:nrow(topmod), topmod$nrfa, type="l", xlab="Time (days)",
+     ylab=expression(Steramflow~m^2/day))
+lines(topmod$Qt, col="red")
+plot(1:1600, topmod$nrfa[1:1600], type="l", xlab="Time (days)",
+     ylab=expression(Steramflow~m^2/day))
+lines(topmod$Qt[1:1600], col="red")
+legend("topleft", c("Observed", "Predicted"), col=c("black", "red"), lty=c(1, 1), bty="o")
+par(mar=c(5, 4, 4, 2) + 0.1) # defaults
 
 topmod_lng <- pivot_longer(topmod, !date, names_to="topmod_output",
                            values_to="output")

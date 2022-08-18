5f729bbfb19894640204b730a48b385ddc0a0bcb
Roy Sanderson <roy.sanderson@ncl.ac.uk>
2022-08-18 15:08:32 +0100

    Re-run calibrated edge catchments-save prediction
    Saves predicted values for edge streams (even though nrfa available
    for these ones). Adapted the quick_compare.R script to plot the
    predicted and observed flows using ggplot2 syntax.

--- quick_compare.R
+++ quick_compare.R
@@ -1,13 +1,26 @@
-topmod_pred <- read.table("topmod_output_for_R.txt", header=TRUE)
-topmod_obs  <- read.table("obs_for_R.txt", col.names="nrfa_obs")
+# Allows quick visual comparison of observations and predictions
+library(lubridate)
+library(ggplot2)
+library(tidyr)
 
-# Need to correct to same length as NRFA sometimes end mid-year
-topmod_pred <- topmod_pred[1:nrow(topmod_obs),]
-topmod <- cbind(topmod_pred, topmod_obs)
+subcatch_no <- 3
+nrfa_no     <- 21027
 
-plot(topmod$Qt, topmod$nrfa_obs)
+preds <- read.table(paste0("subcatch_dat/output.txt.", subcatch_no),
+                    skip=66, header=TRUE)
+nrfa  <- read.table(paste0("subcatch_dat/obs.txt.", nrfa_no))
 
-par(mfrow=c(2,1))
-plot(1:3560, topmod$Qt, type="l")
-plot(1:3560, topmod$nrfa_obs, type="l")
-par(mfrow=c(1,1))
+date  <- seq(ymd('2010-01-01'),ymd('2017-12-31'),by='days')
+
+topmod <- cbind(preds, nrfa, date)
+colnames(topmod)[10] <- "nrfa"
+plot(1:nrow(topmod), topmod$Qt, type="l")
+lines(topmod$nrfa, col="red")
+
+topmod_lng <- pivot_longer(topmod, !date, names_to="topmod_output",
+                           values_to="output")
+topmod_lng %>%
+   filter(topmod_output == "Qt" | topmod_output == "nrfa") %>%
+ggplot(aes(x = date, y=output, colour=topmod_output)) +
+   geom_line() +
+	 theme_classic()
